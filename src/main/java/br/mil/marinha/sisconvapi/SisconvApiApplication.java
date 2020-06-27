c01e6d59c81ba718ef432939a41cb31fa8a25e3c
webmaster-pnn <machadopnn@gmail.com>
2020-06-27 16:13:04 -0300

    Inicio de implementacao da API que será consumida pelo sistema SISCONV.

---
+++ src/main/java/br/mil/marinha/sisconvapi/SisconvApiApplication.java
@@ -0,0 +1,13 @@
+package br.mil.marinha.sisconvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisconvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisconvApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mil/marinha/sisconvapi/SisconvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mil.marinha.sisconvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisconvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
