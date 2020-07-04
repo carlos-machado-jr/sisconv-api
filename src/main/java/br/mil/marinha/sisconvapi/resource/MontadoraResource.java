175908bd0dbd256efe4aadec65d15756c2158544
webmaster-pnn <machadopnn@gmail.com>
2020-07-04 17:32:58 -0300

    Todos os metodos de montadora resource e service implementados

--- src/main/java/br/mil/marinha/sisconvapi/resource/MontadoraResource.java
+++ src/main/java/br/mil/marinha/sisconvapi/resource/MontadoraResource.java
@@ -1,12 +1,18 @@
 package br.mil.marinha.sisconvapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.mil.marinha.sisconvapi.domain.Montadora;
 import br.mil.marinha.sisconvapi.service.MontadoraService;
@@ -16,12 +22,34 @@ import br.mil.marinha.sisconvapi.service.MontadoraService;
 public class MontadoraResource {
 
 	@Autowired
-	MontadoraService service;
+	MontadoraService montadoraService;
 	
 	@GetMapping
 	public ResponseEntity<List<Montadora>> findAll(){
-		List<Montadora> listMontadora = service.findAll();
+		List<Montadora> listMontadora = montadoraService.findAll();
 		
 		return ResponseEntity.ok(listMontadora);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Montadora> findById(@PathVariable Integer id){
+		Montadora montadora = montadoraService.findById(id);
+		return ResponseEntity.ok(montadora);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> create(@RequestBody Montadora montadora){
+		montadoraService.create(montadora);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(montadora.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Montadora montadora){
+		montadora.setId(id);
+		montadoraService.update(montadora);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/br/mil/marinha/sisconvapi/service/MontadoraService.java
+++ src/main/java/br/mil/marinha/sisconvapi/service/MontadoraService.java
@@ -1,24 +1,45 @@
 package br.mil.marinha.sisconvapi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.mil.marinha.sisconvapi.domain.Montadora;
 import br.mil.marinha.sisconvapi.repositories.MontadoraRepository;
+import br.mil.marinha.sisconvapi.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class MontadoraService {
-	
+
 	@Autowired
-	MontadoraRepository repo;
-	
-	public List<Montadora> findAll(){
-		return repo.findAll();
+	MontadoraRepository montadoraRepository;
+
+	public List<Montadora> findAll() {
+		return montadoraRepository.findAll();
 	}
-	
+
 	public Montadora findByDescricao(String descricao) {
-		return repo.findByDescricao(descricao);
+		return montadoraRepository.findByDescricao(descricao);
+	}
+
+	public Montadora findById(Integer id) {
+		Optional<Montadora> montadora = montadoraRepository.findById(id);
+		return montadora.orElseThrow(() -> objectNotFoundException(id));
+	}
+
+	public void create(Montadora montadora) {
+		montadoraRepository.save(montadora);
+	}
+
+	public void update(Montadora montadora) {
+		findById(montadora.getId());
+		montadoraRepository.save(montadora);
+	}
+
+	private ObjectNotFoundException objectNotFoundException(Integer id) {
+		return new ObjectNotFoundException(
+				"Montadora não encontrada!  id: " + id + ", Tipo: " + Montadora.class.getName());
 	}
 }
